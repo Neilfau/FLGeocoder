87424e066b5ef5f505ac1f49148aa307ec34a311
Neil Faulkner <info@faulknerlabs.io>
2017-11-07 16:03:55 +0100

    Initial Commit

---
+++ FLGecoder/FLGecoder.h
@@ -0,0 +1,19 @@
+//
+//  FLGecoder.h
+//  FLGecoder
+//
+//  Created by Neil Faulkner on 07/11/2017.
+//  Copyright © 2017 Faulkner Labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FLGecoder.
+FOUNDATION_EXPORT double FLGecoderVersionNumber;
+
+//! Project version string for FLGecoder.
+FOUNDATION_EXPORT const unsigned char FLGecoderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FLGecoder/PublicHeader.h>
+
+
